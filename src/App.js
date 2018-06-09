704c6286a6fff201613eca2585c0733f4735ad23
avasinis <aurelien.vasinis@brainsfeed.com>
2018-06-09 18:04:16 +0200

    Add MUI & Roboto

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 
+import Demo from './cmpts/stepDemo';
+import Test from './cmpts/test';
+
 Amplify.configure(aws_exports);
 
 
@@ -16,8 +19,10 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+            To get started, edit <code>src/App.js</code> and save to reload.
+            <Test></Test>
         </p>
+        <Demo></Demo>
       </div>
     );
   }
---
+++ src/cmpts/test.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+export default class Test extends Component{
+    render(){
+        return(
+            <Button variant="contained" color="primary">
+                Hello R.A.W
+            </Button>
+        )
+    }
+}
